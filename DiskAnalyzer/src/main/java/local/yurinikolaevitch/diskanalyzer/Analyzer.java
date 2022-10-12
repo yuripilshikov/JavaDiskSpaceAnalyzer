034996bc6e17f8aa91697ba185dffa07ea0bb1c8
Yuri Pilshikov <yuri.nikolaevitch@gmail.com>
2022-10-12 09:53:21 +0300

    Tutorial passed. Next turn is to improve this.

---
+++ DiskAnalyzer/src/main/java/local/yurinikolaevitch/diskanalyzer/Analyzer.java
@@ -0,0 +1,52 @@
+package local.yurinikolaevitch.diskanalyzer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author YuriPilshikov
+ */
+public class Analyzer {
+
+    private Map<String, Long> sizes = new HashMap<>();
+
+    public Map<String, Long> calculateDirectorySize(Path path) {
+        try {
+
+            Files.walkFileTree(path, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    long size = Files.size(file);
+                    updateDirSize(file, size);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            return sizes;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private void updateDirSize(Path path, Long size) {
+        String key = path.toString();
+        sizes.put(key, size + sizes.getOrDefault(key, 0L));
+
+        Path parent = path.getParent();
+        if (parent != null) {
+            updateDirSize(parent, size);
+        }
+    }
+}
